f5fd5638a4dbec829e211cd20e69a9f64242fa09
sahilhemnani <amrc66283@gmail.com>
2024-09-13 17:52:16 +0530

    added code

---
+++ locust1.py
@@ -0,0 +1,154 @@
+from locust import HttpUser, TaskSet, task, between
+import time
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        self.username = "sahil@dglide.com"
+        self.password = "sahil@dglide.com"
+        
+        # Perform login and store the token
+        self.token = self.login()
+        self.headers = {
+            "Authorization": f"Bearer {self.token}",
+        }
+        
+        # Variable to store the last created ticket's ID
+        self.created_ticket_id = None
+
+    def rest_api_call(self, url, method, json_body=None, headers=None):
+        if method.lower() == "post":
+            response = self.client.post(url, json=json_body, headers=headers)
+        elif method.lower() == "put":
+            response = self.client.put(url, json=json_body, headers=headers)
+        elif method.lower() == "get":
+            response = self.client.get(url, headers=headers)
+        elif method.lower() == "delete":
+            response = self.client.delete(url, headers=headers, json = json_body)
+        else:
+            raise ValueError("Unsupported HTTP method")
+        
+        return response.json()
+
+    
+    def login(self):
+        """ Function to log in and return the token. """
+        url = "/api/v1/user/login"
+        json_body = {
+            "username": self.username,
+            "password": self.password
+        }
+        response = self.rest_api_call(url, "post", json_body=json_body)
+        return response.get("result", {}).get("tokenDetail", {}).get("token")
+
+    @task
+    def getAllModules(self):
+        url = "/api/v1/module"  
+        response = self.rest_api_call(url, "get", headers=self.headers)
+        print(response.get("statusCode"))
+        
+    @task
+    def fetchStarDeskModule(self):
+        url = "/api/v1/form/module/39"  
+        response = self.rest_api_call(url, "get", headers=self.headers)
+        print(response.get("statusCode"))
+        
+    @task
+    def getTicketDetails(self):
+        url = "/api/v1/form/name/ticket"  
+        response = self.rest_api_call(url, "get", headers=self.headers)
+        print(response.get("statusCode"))
+        
+    @task
+    def xyz(self):
+        url = "/api/v1/field/form/258"  
+        response = self.rest_api_call(url, "get", headers=self.headers)
+        print(response.get("statusCode"))
+
+    @task
+    def pqr(self):
+        url = "/api/v1/action/form/258"  
+        response = self.rest_api_call(url, "get", headers=self.headers)
+        print(response.get("statusCode"))
+
+    @task
+    def asa(self):
+        url = "/api/v1/field/form/258"  
+        response = self.rest_api_call(url, "get", headers=self.headers)
+        print(response.get("statusCode"))
+
+    @task
+    def search(self):
+        url = "/api/v1/table/ticket/data/search"  
+        response = self.rest_api_call(url, "post",
+                                      json_body= {
+                                                    "pagination": {
+                                                        "pageSize": 20,
+                                                        "page": 0
+                                                    },
+                                                    "where": [],
+                                                    "sort": []
+                                                    }, headers=self.headers)
+        print(response.get("statusCode"))
+
+        
+    @task
+    def getPreferences(self):
+        url = "/api/v1/form/258/field/preference"  
+        response = self.rest_api_call(url, "get", headers=self.headers)
+        print(response.get("statusCode"))
+
+
+    @task
+    def updateTicketActual(self):
+        for i in range(10):
+            url = "https://prod-api.dglide.com/api/v1/table/ticket/data/update/0f401187-0796-40f7-9604-f34367ea9d32"  
+            response = self.rest_api_call(url, "put",
+                                        json_body= {
+                                                    "status": "1",
+                                                    "priority": "1",
+                                                    "source": "1",
+                                                    "requester": "969e3611-d515-4520-b286-cf531043b31c",
+                                                    "summary": str(i) + "th time updating the ticket",
+                                                    }, headers=self.headers)
+            print(response.get("statusCode"))
+            print("ticket updated")
+
+    @task
+    def create_ticket(self):
+        """ Function to create a ticket via API. """
+        url = "/api/v1/table/ticket/data/save"
+        json_body = {
+            "subject": "summary",
+            "description": "locust load testing going on..."
+        }
+       
+        response = self.rest_api_call(url, "post", json_body=json_body, headers=self.headers)
+        self.created_ticket_id = response.get("result").get("uuid")
+        print(f"Created Ticket ID: {self.created_ticket_id}")
+
+    @task
+    def delete_ticket(self):
+        """ Function to delete the last created ticket via API. """
+        if self.created_ticket_id:
+            url = "/api/v1/table/ticket/data"
+            listOfTickets = []
+            listOfTickets.append(self.created_ticket_id)
+            response = self.rest_api_call(url, "delete", headers=self.headers, json_body = listOfTickets)
+            self.created_ticket_id = None  # Reset the ticket ID after deletion
+            print("deleted successfully")
+
+    @task
+    def run_ticket_cycle(self):
+        """ Run the ticket creation and deletion in a cycle every minute. """
+        if self.created_ticket_id:
+            self.delete_ticket()
+        self.create_ticket()
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 5)
+
+    def tick(self):
+        current_time = int(time.time())
+        if current_time % 60 == 0:
+            self.user_behavior.run_ticket_cycle()
---
+++ locust3.py
@@ -0,0 +1,56 @@
+from locust import HttpUser, TaskSet, task, between
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        """ Perform login when a user starts. """
+        # Define single user's credentials
+        self.username = "tenant3@example.com"
+        self.password = "sysadmin"
+        self.tenant_id = "tenant3"
+        
+        # Perform login and store the token
+        self.token = self.login()
+        self.headers = {
+            "Authorization": f"Bearer {self.token}",
+            "X-TenantID": self.tenant_id
+        }
+
+    def rest_api_call(self, url, method, json_body=None, headers=None):
+        """ General function to make REST API calls. """
+        if method.lower() == "post":
+            response = self.client.post(url, json=json_body, headers=headers)
+        elif method.lower() == "get":
+            response = self.client.get(url, headers=headers)
+        else:
+            raise ValueError("Unsupported HTTP method")
+        
+        return response.json()
+
+    def login(self):
+        """ Function to log in and return the token. """
+        url = "/api/v1/user/login"
+        json_body = {
+            "username": self.username,
+            "password": self.password
+        }
+        headers = {
+            "X-TenantID": self.tenant_id
+        }
+        response = self.rest_api_call(url, "post", json_body=json_body, headers=headers)
+        return response.get("result", {}).get("tokenDetail", {}).get("token")
+
+    @task
+    def api1(self):
+        url = "/api/v1/module"  # Replace with your actual endpoint
+        response = self.rest_api_call(url, "get", headers=self.headers)
+        print(response)  # Handle the response as needed
+    
+    # @task
+    # def api2(self):
+    #     url = "/api/v1/"  # Replace with your actual endpoint
+    #     response = self.rest_api_call(url, "get", headers=self.headers)
+    #     print(response)
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 5)
